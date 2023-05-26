dff103c5a3773661ce68fc6fc87d255822b35d4c
clementinise <35346472+clementinise@users.noreply.github.com>
2023-05-26 15:17:06 +0200

    V1.3

--- config.lua
+++ config.lua
@@ -1,9 +1,10 @@
-Config = Config or {}
+Config                      = Config or {}
 
-Config.Eject = true -- If set to true, player will be ejected if they land while doing a trick
+Config.Eject                = true      -- This value determines whether the player will be ejected if they land while doing a trick or not (The default value is true)
+Config.InstantAnimStop      = true      -- This value determines whether the anim should instantly stopped, otherwise it will just go back to the sitting position gradually (The default value is true)
 
-Config.SpecificVehicle = true -- If set to true, you will be able to do tricks only on some specific bikes from the 'Config.Vehicles' list
-Config.Vehicles = { -- Don't use "Model Name" since there is no native to get model name from vehicle in GTA, only use "Text Label Name(s)". You can found them here: https://gta.fandom.com/wiki/Category:Motorcycles_Vehicle_Class
+Config.SpecificVehicle      = true      -- If set to true, you will be able to do tricks only on some specific bikes from the 'Config.Vehicles' list
+Config.Vehicles             = {         -- Don't use "Model Name" since there is no native to get model name from vehicle in GTA, only use "Text Label Name(s)". You can found them here: https://gta.fandom.com/wiki/Category:Motorcycles_Vehicle_Class
     "ENDURO",
     "ESSKEY",
     "MANCHEZ",
@@ -11,6 +12,5 @@ Config.Vehicles = { -- Don't use "Model Name" since there is no native to get mo
     "SANCHEZ02"
 }
 
-Config.SimpleKey = 76 -- Set the key used for the simple tricks (76 is 'INPUT_VEH_HANDBRAKE' aka SPACEBAR per default)
-Config.OtherKey = 73 -- Set the key used for other tricks (73 is 'INPUT_VEH_DUCK' aka X per default)
+Config.SimpleKey            = 76        -- Set the key used for the simple tricks (76 is 'INPUT_VEH_HANDBRAKE' aka SPACEBAR per default)
 -- You can find those value here: https://docs.fivem.net/docs/game-references/controls/
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,17 @@
 fx_version 'cerulean'
 game 'gta5'
 
+lua54 'yes'
+
 author 'Clementinise'
+name 'KC Tricks'
 description 'Tricks resource for FiveM servers'
-version '1.0'
+github 'https://github.com/clementinise/kc-tricks'
+version '1.1'
 
 client_scripts {
 	'config.lua',
 	'client/main.lua'
 }
+
+fivem_checker 'yes'
